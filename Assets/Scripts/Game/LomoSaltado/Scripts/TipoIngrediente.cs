323865c515516a8854635b3a2417c20933354f8e
victorcollazosisnull <victor.collazos@tecsup.edu.pe>
2025-05-01 21:46:57 -0500

    Ceviche

---
+++ Assets/Scripts/Game/LomoSaltado/Scripts/TipoIngrediente.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public enum TipoIngrediente
+{
+    //LOMO SALTADO
+    Papa,
+    Cebolla,
+    Tomate,
+    AjiAmarillo,
+    Carne,
+    PapaCortada,
+    CarneCortada,
+    Arroz,
+    Cuchillo,
+    CebollaCortada,
+    TomateCortado,
+    AjiAmarilloCortado,
+    Premix,
+    Nage,
+    Salsa,
+    Culantro,
+    CarneEnSarten,
+    PapaEnSarten,
+    OllaConArroz,
+    //CEVICHE
+    Pescado,
+    Rocoto,
+    AjiLimo,
+}
